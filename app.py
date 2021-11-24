edeae6c9229cfd351e2ef710f148a813a7698610
aadesh.agarwal8888 <aadesh.agarwal8888@gmail.com>
2021-11-24 07:02:53 -0500

    / added

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ collection = db["users"]
 
 app = Flask(__name__)
 CORS(app)
+@app.route("/")
+def root():
+    return "hello"
+
 @app.route('/weather', methods = ["POST"])
 def get_weather():
     request_data = request.get_json()
